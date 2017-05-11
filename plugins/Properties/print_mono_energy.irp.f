b81aae00776b8d9a123de5dbe33f2be8466576b7
Emmanuel Giner <giner.emmanuel@gmail.com>
2017-05-11 16:35:05 +0200

    mend

--- plugins/Properties/print_mono_energy.irp.f
+++ plugins/Properties/print_mono_energy.irp.f
@@ -12,10 +12,10 @@ subroutine routine
  call get_average(mo_mono_elec_integral,one_body_dm_mo,average)
  print*, 'density mono energy = ',average
  print*, '**********************'
- print*, 'psi_energy_bielec   = ',psi_energy_bielec
- print*, 'psi_energy_monoelec = ',psi_energy_monoelec
- print*, 'total energy        = ',psi_energy_monoelec + psi_energy_bielec
- print*, 'psi_energy          = ',psi_energy
+ print*, 'psi_energy_bielec   = ',psi_energy_bielec(1)
+ print*, 'psi_energy_monoelec = ',psi_energy_monoelec(1)
+ print*, 'total energy        = ',psi_energy_monoelec(1) + psi_energy_bielec(1)
+ print*, 'psi_energy          = ',psi_energy(1)
  
 
 end
