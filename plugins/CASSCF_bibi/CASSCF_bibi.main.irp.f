95c585bd37576d00574a17124bca4813209d9626
Emmanuel Giner <eginer@vortex01.bc.rzg.mpg.de>
2017-09-26 16:10:39 +0200

    Partial occ-virt transformation works

--- plugins/CASSCF_bibi/CASSCF_bibi.main.irp.f
+++ plugins/CASSCF_bibi/CASSCF_bibi.main.irp.f
@@ -1,5 +1,10 @@
 program CASSCF_bibi
   implicit none
+  call routine1
+
+
+end
+subroutine routine1
   integer :: i,iorb,j,jorb,k,korb,l,lorb
 ! do i = 1, n_core_inact_orb
 !  iorb = list_core_inact(i)
@@ -20,6 +25,47 @@ program CASSCF_bibi
 ! enddo
 !enddo
 !call iteration_scf
+
+ integer :: n,m
+ double precision :: accu,ao_bielec_integral,get_ao_bielec_integral
+ double precision :: integral_map,integral_ao
+ double precision :: matrix_tmp_1(n_core_inact_orb,ao_num),matrix_final(n_core_inact_orb,n_virt_orb)
+ double precision :: vec_1(ao_num),vec_2(ao_num),vec_3(ao_num),accu_2,bielec_tmp_0(ao_num,ao_num)
+ double precision :: vec_4(ao_num),accu_3
+ do k = 1, ao_num
+  do l = 1, ao_num
+   matrix_tmp_1 = 0.d0
+   matrix_final = 0.d0
+   do m = 1, ao_num
+    call get_ao_bielec_integrals(k,m,l,ao_num,bielec_tmp_0(1,m)) ! all integrals for a given l1, k1
+   enddo
+   call dgemm('N','N',n_core_inact_orb,ao_num,ao_num,1.d0,mo_coef_core_inact_transp,n_core_inact_orb,bielec_tmp_0,ao_num,0.d0,matrix_tmp_1,n_core_inact_orb)
+   call dgemm('N','N',n_core_inact_orb,n_virt_orb,ao_num,1.d0,matrix_tmp_1,n_core_inact_orb,mo_coef_virt,ao_num,0.d0,matrix_final,n_core_inact_orb)
+   do i = 1, n_core_inact_orb
+    iorb = list_core_inact(i)
+    do j = 1, n_virt_orb
+     jorb = list_virt(j)
+      accu_2 = 0.d0
+      do m = 1, ao_num
+       call get_ao_bielec_integrals(k,m,l,ao_num,bielec_tmp_0(1,m)) ! all integrals for a given l1, k1
+      enddo
+      do m = 1, ao_num
+       do n = 1, ao_num
+        accu_2 += bielec_tmp_0(m,n) * mo_coef(n,jorb) * mo_coef(m,iorb)
+       enddo
+      enddo
+      if(dabs(accu_2-semi_transformed_occ_virt(k,l,i,j)).gt.1.d-10)then
+       print*, 'pb !!'
+       print*, k,l,i,j
+       print*, accu_2,semi_transformed_occ_virt(k,l,i,j)
+       stop
+      endif
+    enddo
+   enddo
+  enddo
+ enddo
+
+
  double precision, allocatable :: matrix_integrals(:,:)
  double precision :: get_mo_bielec_integral,integral
  allocate(matrix_integrals(n_core_inact_orb, n_virt_orb))
@@ -41,4 +87,19 @@ program CASSCF_bibi
    enddo
   enddo
  enddo
+end
+
+subroutine routine2
+  integer :: i,iorb,j,jorb,k,korb,l,lorb
+  double precision, allocatable :: matrix_integrals(:,:)
+  double precision :: get_mo_bielec_integral,integral
+  allocate(matrix_integrals(n_core_inact_orb, n_virt_orb))
+  iorb = 1
+  jorb = 6 
+  i = list_core_inact_reverse(iorb)
+  j = list_virt_reverse(jorb)
+  print*, '------'
+  print*, i,j
+  print*, '---------'
+  call get_all_core_inact_virt_integrals(i,j,matrix_integrals)
 end
--- plugins/CASSCF_bibi/integrals_core_inact_virt.irp.f
+++ plugins/CASSCF_bibi/integrals_core_inact_virt.irp.f
@@ -51,66 +51,38 @@ END_PROVIDER
 END_PROVIDER 
 
 
-BEGIN_PROVIDER [real(integral_kind), semi_transformed_occ_virt, (ao_num,ao_num,n_core_inact_orb,n_virt_orb)]
+ BEGIN_PROVIDER [real(integral_kind), semi_transformed_occ_virt, (ao_num,ao_num,n_core_inact_orb,n_virt_orb)]
+&BEGIN_PROVIDER [real(integral_kind), semi_transformed_occ_virt_bis, (ao_num,ao_num,n_core_inact_orb,n_virt_orb)]
   use map_module
  implicit none
  semi_transformed_occ_virt = 0.d0
- integer :: i,j,k,l,iorb,jorb,korb,lorb,m
- integer :: i1,j1,k1,l1,kmax
- double precision :: c
- real(integral_kind), allocatable :: bielec_tmp_0(:,:)
- integer, allocatable           :: bielec_tmp_0_idx(:)
- double precision, allocatable :: mo_coef_one_column(:),mo_coef_one_column_bis(:),contraction_tmp(:)
- double precision :: u_dot_v
- double precision :: tmp(ao_num)
- allocate(bielec_tmp_0(ao_num,ao_num),bielec_tmp_0_idx(ao_num),mo_coef_one_column(ao_num_align),contraction_tmp(ao_num_align),mo_coef_one_column_bis(ao_num_align))
 
- do l1 = 1,ao_num
-   !DEC$ VECTOR ALIGNED
-   do k1 = 1,ao_num
-     !DEC$ VECTOR ALIGNED
-     do j1 = 1,ao_num
-       call get_ao_bielec_integrals(k1,j1,l1,ao_num,bielec_tmp_0(1,j1)) ! all integrals for a given l1, k1
-     enddo
+ semi_transformed_occ_virt_bis = 0.d0
+
+ integer :: i,j,k,l,iorb,jorb,korb,lorb,m,n
+ double precision :: c
+ real(integral_kind), allocatable :: bielec_tmp_0(:,:),matrix_tmp_1(:,:),matrix_final(:,:)
+ real(integral_kind) :: integral,ao_bielec_integral
+
+ allocate(bielec_tmp_0(ao_num,ao_num),matrix_tmp_1(n_core_inact_orb,ao_num),matrix_final(n_core_inact_orb,n_virt_orb))
+
+ do k = 1,ao_num
+   do l = 1,ao_num
+    matrix_tmp_1 = 0.d0
+    matrix_final = 0.d0
+    do m = 1, ao_num
+     call get_ao_bielec_integrals(k,m,l,ao_num,bielec_tmp_0(1,m)) ! k,l :: r1, m :: r2
+    enddo
+    call dgemm('N','N',n_core_inact_orb,ao_num,ao_num,1.d0,mo_coef_core_inact_transp,n_core_inact_orb,bielec_tmp_0,ao_num,0.d0,matrix_tmp_1,n_core_inact_orb)
+    call dgemm('N','N',n_core_inact_orb,n_virt_orb,ao_num,1.d0,matrix_tmp_1,n_core_inact_orb,mo_coef_virt,ao_num,0.d0,matrix_final,n_core_inact_orb)
+    do j = 1, n_virt_orb
      do i = 1, n_core_inact_orb
-      iorb = list_core_inact(i)
-      do k = 1, ao_num
-       mo_coef_one_column(k) = mo_coef(k,iorb)
-       contraction_tmp(k) = 0.d0
-      enddo
-!     tmp = 0.d0
-!     do m = 1, ao_num
-!      do k = 1, ao_num
-!       tmp(m) += bielec_tmp_0(m,k) * mo_coef_one_column(k)
-!      enddo
-!     enddo
-      call matrix_vector_product(mo_coef_one_column,contraction_tmp,bielec_tmp_0,ao_num,ao_num)
-!     do m = 1, ao_num
-!      print*, m,'m'
-!      if(dabs(tmp(m) - contraction_tmp(m)).gt.1.d-10)then
-!       print*, tmp(m),contraction_tmp(m),dabs(tmp(m) - contraction_tmp(m))
-!      endif
-!     enddo
-      
-      do j = 1, n_virt_orb 
-       jorb = list_virt(i)
-       double precision :: accu 
-       accu = 0.d0
-       do k = 1, ao_num
-        mo_coef_one_column_bis(k) = mo_coef(k,jorb)
-        accu += mo_coef(k,jorb) * contraction_tmp(k)
-       enddo
-       
-       semi_transformed_occ_virt(l1,k1,i,j) = u_dot_v(mo_coef_one_column_bis,contraction_tmp,ao_num)
-       if(dabs(accu - semi_transformed_occ_virt(l1,k1,i,j)).gt.1.d-10)then
-        print*, accu,semi_transformed_occ_virt(l1,k1,i,j),dabs(accu - semi_transformed_occ_virt(l1,k1,i,j))
-       endif
-      enddo
+      semi_transformed_occ_virt(k,l,i,j) = matrix_final(i,j)
      enddo
+    enddo
    enddo
   enddo
-
-
+ 
 END_PROVIDER 
 
  BEGIN_PROVIDER [double precision, mo_coef_core_inact, (ao_num, n_core_inact_orb)]
@@ -145,22 +117,20 @@ subroutine get_all_core_inact_virt_integrals(iorb,vorb,matrix_integrals)
   use map_module
  implicit none
  integer, intent(in) :: iorb,vorb
- integer :: iorb_i,vorb_v
  real(integral_kind), intent(out) :: matrix_integrals(n_core_inact_orb,n_virt_orb)
  real(integral_kind) :: integrals_matrix(ao_num,ao_num)
- real(integral_kind) :: integrals_tmp(n_core_inact_orb,ao_num)
- integer :: i,j,k,l
-!iorb_i = list_core_inact_reverse(iorb_i)
-!vorb_v = list_virt_reverse(vorb_v)
- do i = 1, ao_num
-  do j = 1, ao_num
-   integrals_matrix(i,j) = semi_transformed_occ_virt(j,i,iorb,vorb)
+ real(integral_kind) :: matrix_tmp_1(n_core_inact_orb,ao_num)
+ integer :: i,j
+ integer :: m,n
+
+ do m = 1, ao_num
+  do n = 1, ao_num
+   integrals_matrix(m,n) = semi_transformed_occ_virt(m,n,iorb,vorb)
   enddo
  enddo
+ matrix_integrals = 0.d0
  
- ! integrals_tmp = C integrals_matrix  
- call dgemm( 'N','N',n_core_inact_orb,ao_num,ao_num, 1.d0,mo_coef_core_inact_transp,n_core_inact_orb,integrals_matrix,ao_num,1.d0,integrals_tmp,n_core_inact_orb)
- ! matrix_integrals = integrals_tmp C^+
- call dgemm( 'N','N',n_core_inact_orb,n_virt_orb,ao_num, 1.d0,integrals_tmp,n_core_inact_orb,mo_coef_virt,ao_num,1.d0,matrix_integrals,n_virt_orb)
+ call dgemm('N','N',n_core_inact_orb,ao_num,ao_num,1.d0,mo_coef_core_inact_transp,n_core_inact_orb,integrals_matrix,ao_num,0.d0,matrix_tmp_1,n_core_inact_orb)
+ call dgemm('N','N',n_core_inact_orb,n_virt_orb,ao_num,1.d0,matrix_tmp_1,n_core_inact_orb,mo_coef_virt,ao_num,0.d0,matrix_integrals,n_core_inact_orb)
 
 end
