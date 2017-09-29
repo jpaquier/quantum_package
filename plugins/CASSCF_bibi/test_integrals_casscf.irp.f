7eef742c78221a6a513c0af267ff9ad874958c25
Emmanuel Giner <eginer@vortex01.bc.rzg.mpg.de>
2017-09-29 13:09:38 +0200

    All integrals for occ-virt Single Ref SUPER-CI are ok

--- plugins/CASSCF_bibi/test_integrals_casscf.irp.f
+++ plugins/CASSCF_bibi/test_integrals_casscf.irp.f
@@ -4,6 +4,7 @@ program CASSCF_bibi
   call check_virt1_core2_core2 
   call check_core1_core2_virt2
   call check_core1_virt1_core2_virt2
+  call check_core1_core1_virt2_virt2
 end 
 
 subroutine check_core1_virt2_virt2
@@ -107,5 +108,34 @@ subroutine check_core1_virt1_core2_virt2
  enddo
  
  print*, 'passed check_core1_virt1_core2_virt2'
+
+end
+
+subroutine check_core1_core1_virt2_virt2
+ implicit none
+
+ double precision :: get_mo_bielec_integral,integral
+ integer :: i,j,k,l
+ integer :: iorb,jorb,korb,lorb
+ do i = 1, n_core_inact_orb
+  iorb = list_core_inact(i)
+  do j = 1, n_core_inact_orb
+   jorb = list_core_inact(j)
+   do k = 1, n_virt_orb
+    korb = list_virt(k)
+    do l = 1, n_virt_orb
+     lorb = list_virt(l)
+     integral = get_mo_bielec_integral(iorb,korb,jorb,lorb,mo_integrals_map)
+     if(dabs(transformed_occ1_occ1_virt2_virt2(k,l,i,j) - integral ).gt.1.d-10)then
+      print*, 'pb in fully transformed'
+      print*, iorb,jorb,korb
+      print*, integral,transformed_occ1_occ1_virt2_virt2(k,l,i,j),dabs(transformed_occ1_occ1_virt2_virt2(k,l,i,j) - integral )
+     endif
+    enddo
+   enddo
+  enddo
+ enddo
+ 
+ print*, 'passed check_core1_core1_virt2_virt2'
 
 end
