69770760313469e67b1c6132c77193f33dcca01d
Anthony Ferté <aferte@thaumaste.lct.jussieu.fr>
2018-02-16 17:03:29 +0100

    Added HF_energy in Hartree_Fock/Fock_matrix.irp.f

--- plugins/Hartree_Fock/Fock_matrix.irp.f
+++ plugins/Hartree_Fock/Fock_matrix.irp.f
@@ -182,3 +182,21 @@ BEGIN_PROVIDER [double precision, extra_energy_contrib_from_density]
 
 END_PROVIDER 
 
+ BEGIN_PROVIDER [ double precision, HF_energy]
+&BEGIN_PROVIDER [ double precision, HF_two_electron_energy]
+&BEGIN_PROVIDER [ double precision, HF_one_electron_energy]
+ implicit none
+ integer :: i,j 
+ HF_energy = nuclear_repulsion
+ do j=1,ao_num
+   do i=1,ao_num
+    HF_two_electron_energy += 0.5d0 * ( ao_bi_elec_integral_alpha(i,j) * SCF_density_matrix_ao_alpha(i,j) &
+                                       +ao_bi_elec_integral_beta(i,j)  * SCF_density_matrix_ao_beta(i,j) )
+    HF_one_electron_energy += ao_mono_elec_integral(i,j) * (SCF_density_matrix_ao_alpha(i,j) + SCF_density_matrix_ao_beta (i,j) )
+   enddo
+ enddo
+ HF_energy += HF_two_electron_energy + HF_one_electron_energy
+
+
+END_PROVIDER 
+
