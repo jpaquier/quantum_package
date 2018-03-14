f1d57d3aa322091af9fdfdbb482228a91ac84475
Emmanuel Giner <giner.emmanuel@gmail.com>
2018-03-14 13:59:35 +0100

    problems with LDA

--- plugins/DFT_Utils/grid_density.irp.f
+++ plugins/DFT_Utils/grid_density.irp.f
@@ -4,8 +4,9 @@ BEGIN_PROVIDER [integer, n_points_integration_angular]
 ! number of angular points per atom for 3d numerical integration, needed for DFT for example
  END_DOC
 !n_points_integration_angular = 266
- n_points_integration_angular = 590
+!n_points_integration_angular = 590
 !n_points_integration_angular =1202
+ n_points_integration_angular = 2030
 !n_points_integration_angular = 5810
 END_PROVIDER 
 
@@ -36,9 +37,9 @@ END_PROVIDER
  accu = 0.d0
  double precision :: x(n_points_integration_angular),y(n_points_integration_angular),z(n_points_integration_angular),w(n_points_integration_angular)
 !call LD5810(X,Y,Z,W,n_points_integration_angular)
-!call LD2030(X,Y,Z,W,n_points_integration_angular)
+ call LD2030(X,Y,Z,W,n_points_integration_angular)
 !call LD1202(X,Y,Z,W,n_points_integration_angular)
- call LD0590(X,Y,Z,W,n_points_integration_angular)
+!call LD0590(X,Y,Z,W,n_points_integration_angular)
 !call LD0266(X,Y,Z,W,n_points_integration_angular)
  do i = 1, n_points_integration_angular
   angular_quadrature_points(i,1) = x(i)
