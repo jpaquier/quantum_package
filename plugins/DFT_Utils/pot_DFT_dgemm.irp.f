4af6a2d3af75f620be71a274ed5c7963ea099b4a
Anthony Ferté <aferte@master.lct.jussieu.fr>
2018-03-16 13:21:53 +0100

    dgemm added to LDA and GGA potential provider

--- plugins/DFT_Utils/pot_DFT_dgemm.irp.f
+++ plugins/DFT_Utils/pot_DFT_dgemm.irp.f
@@ -6,7 +6,7 @@
 &BEGIN_PROVIDER [double precision, potential_c_beta_ao_new,(ao_num,ao_num,N_states)]
 
  implicit none
- integer :: j,k,l,istate
+ integer :: i,j,k,l,istate
  integer :: m,n,p,q
  double precision, allocatable :: aos_array(:)
  double precision, allocatable :: r(:)
@@ -28,8 +28,9 @@
   energy_c_new = 0.d0
   potential_c_alpha_ao_new = 0.d0
   potential_c_beta_ao_new = 0.d0
-  potential_x_alpha_ao_new_new = 0.d0
+  potential_x_alpha_ao_new = 0.d0
   potential_x_beta_ao_new = 0.d0
+
   do j = 1, nucl_num
    do k = 1, n_points_radial_grid  -1
     allocate(tmp_c_a(ao_num,ao_num,N_states),tmp_c_b(ao_num,ao_num,N_states),tmp_x_a(ao_num,ao_num,N_states),tmp_x_b(ao_num,ao_num,N_states),aos_array(ao_num),r(3))
@@ -37,14 +38,17 @@
     tmp_c_b = 0.d0
     tmp_x_a = 0.d0
     tmp_x_b = 0.d0
-    do l = 1, n_points_integration_angular 
-     r(1) = grid_points_per_atom(1,l,k,j)
-     r(2) = grid_points_per_atom(2,l,k,j)
-     r(3) = grid_points_per_atom(3,l,k,j)
-     weight = final_weight_functions_at_grid_points(l,k,j)
-     if(DFT_TYPE=="LDA")then
+
+
+    if(DFT_TYPE=="LDA")then
+     do l = 1, n_points_integration_angular 
+      r(1) = grid_points_per_atom(1,l,k,j)
+      r(2) = grid_points_per_atom(2,l,k,j)
+      r(3) = grid_points_per_atom(3,l,k,j)
+      weight = final_weight_functions_at_grid_points(l,k,j)
       call dm_dft_alpha_beta_and_all_aos_at_r(r,rho_a,rho_b,aos_array)
       call LDA_type_functional(rho_a,rho_b,vx_rho_a,vx_rho_b,vc_rho_a,vc_rho_b,ex,ec)
+
       do istate = 1, N_states
        energy_x_new(istate) += weight *  ex(istate) 
        energy_c_new(istate) += weight *  ec(istate)
@@ -52,50 +56,112 @@
        vc_rho_a(istate) *= weight
        vx_rho_b(istate) *= weight
        vc_rho_b(istate) *= weight
+       double precision :: ao_matrix_vc_a(ao_num,n_point_integration_angular,N_states) 
+       double precision :: ao_matrix_vx_a(ao_num,n_point_integration_angular,N_states) 
+       double precision :: ao_matrix_vc_b(ao_num,n_point_integration_angular,N_states) 
+       double precision :: ao_matrix_vx_b(ao_num,n_point_integration_angular,N_states) 
+       do i = 1 , ao_num 
+        ao_matrix_vc_a(i,l,istate) = vc_rho_a(istate) * aos_array(i)
+        ao_matrix_vc_b(i,l,istate) = vc_rho_b(istate) * aos_array(i)
+        ao_matrix_vx_a(i,l,istate) = vx_rho_a(istate) * aos_array(i)
+        ao_matrix_vx_b(i,l,istate) = vx_rho_b(istate) * aos_array(i)
+       enddo
       enddo
-      call update_potentials_scalar_dger(tmp_c_a,tmp_c_b,tmp_x_a,tmp_x_b,vc_rho_a,vc_rho_b,vx_rho_a,vx_rho_b,aos_array)
+      do istate = 1 , N_states
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1d0,ao_matrix_vc_a(1,1,istate),size(ao_matrix_vc_a,1),ao_matrix,size(ao_matrix,1),0.d0,potential_c_alpha_ao_new(1,1,istate),size(potential_c_alpha_ao_new,1))
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1d0,ao_matrix_vc_b(1,1,istate),size(ao_matrix_vc_b,1),ao_matrix,size(ao_matrix,1),0.d0,potential_c_beta_ao_new(1,1,istate),size(potential_c_beta_ao_new,1))
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1d0,ao_matrix_vx_a(1,1,istate),size(ao_matrix_vx_a,1),ao_matrix,size(ao_matrix,1),0.d0,potential_x_alpha_ao_new(1,1,istate),size(potential_x_alpha_ao_new,1))
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1d0,ao_matrix_vx_b(1,1,istate),size(ao_matrix_vx_b,1),ao_matrix,size(ao_matrix,1),0.d0,potential_x_beta_ao_new(1,1,istate),size(potential_x_beta_ao_new,1))
+      enddo
+     enddo 
 
      else if (DFT_TYPE=="GGA")then
-      call density_and_grad_alpha_beta_and_all_aos_and_grad_aos_at_r(r,rho_a,rho_b, grad_rho_a, grad_rho_b, aos_array, grad_aos_array)
-      grad_rho_a_2 = 0.d0
-      grad_rho_b_2 = 0.d0
-      grad_rho_a_b = 0.d0
-      do istate = 1, N_states
-       do m = 1, 3
-        grad_rho_a_2(istate) += grad_rho_a(m,istate)*grad_rho_a(m,istate)
-        grad_rho_b_2(istate) += grad_rho_b(m,istate)*grad_rho_b(m,istate)
-        grad_rho_a_b(istate) += grad_rho_a(m,istate)*grad_rho_b(m,istate)
+      do l = 1, n_points_integration_angular 
+       r(1) = grid_points_per_atom(1,l,j_rad,i_nucl)
+       r(2) = grid_points_per_atom(2,l,j_rad,i_nucl)
+       r(3) = grid_points_per_atom(3,l,j_rad,i_nucl)
+       call density_and_grad_alpha_beta_and_all_aos_and_grad_aos_at_r(r,rho_a,rho_b, grad_rho_a, grad_rho_b, aos_array, grad_aos_array)
+       grad_rho_a_2 = 0.d0
+       grad_rho_b_2 = 0.d0
+       grad_rho_a_b = 0.d0
+       do istate = 1, N_states
+        do m = 1, 3
+        grad_rho_a_2(istate) += grad_rho_a(m,istate) * grad_rho_a(m,istate)
+        grad_rho_b_2(istate) += grad_rho_b(m,istate) * grad_rho_b(m,istate)
+        grad_rho_a_b(istate) += grad_rho_a(m,istate) * grad_rho_b(m,istate)
+        enddo
        enddo
-      enddo
-      call GGA_type_functionals(rho_a,rho_b,grad_rho_a_2,grad_rho_b_2,grad_rho_a_b,ex,vx_rho_a,vx_rho_b,vx_grad_rho_a_2,vx_grad_rho_b_2,vx_grad_rho_a_b, &  
-                                                                                   ec,vc_rho_a,vc_rho_b,vc_grad_rho_a_2,vc_grad_rho_b_2,vc_grad_rho_a_b )
-      do istate = 1, N_states
-       vx_rho_a(istate) *= weight
-       vc_rho_a(istate) *= weight
-       vx_rho_b(istate) *= weight
-       vc_rho_b(istate) *= weight
-       energy_x_new(istate) += weight *  ex(istate) 
-       energy_c_new(istate) += weight *  ec(istate)
-       do m = 1, 3
-        dtmp_x_a(m,istate) = (2.d0 * vx_grad_rho_a_2(istate) *  grad_rho_a(m,istate) + vx_grad_rho_a_b(istate)  * grad_rho_b(m,istate)) * weight
-        dtmp_x_b(m,istate) = (2.d0 * vx_grad_rho_b_2(istate) *  grad_rho_b(m,istate) + vx_grad_rho_a_b(istate)  * grad_rho_a(m,istate)) * weight
-       enddo
-      enddo
-      call update_potentials_scalar_dger(tmp_c_a,tmp_c_b,tmp_x_a,tmp_x_b,vc_rho_a,vc_rho_b,vx_rho_a,vx_rho_b,aos_array)
-      call dtranspose(grad_aos_array,3,grad_aos_array_transpose,ao_num,3,ao_num)
-      call update_potentials_gradient_dger(dtmp_x_a,dtmp_x_b,dtmp_c_a,dtmp_c_b,aos_array,grad_aos_array_transpose,tmp_x_a,tmp_x_b,tmp_c_a,tmp_c_b)
-     endif
-    enddo ! angular points 
+       call GGA_type_functionals(rho_a,rho_b,∇rho_a_2,∇rho_b_2,∇rho_a_b,ex,vx_rho_a,vx_rho_b,vx_grad_rho_a_2,vx_grad_rho_b_2,vx_grad_rho_a_b, &  
+                                                                                  ec,vc_rho_a,vc_rho_b,vc_grad_rho_a_2,vc_grad_rho_b_2,vc_grad_rho_a_b )
 
-    do istate = 1,N_states
-     potential_c_alpha_ao_new_new(:,:,istate) = potential_c_alpha_ao_new_new(:,:,istate) + tmp_c_a(:,:,istate)
-     potential_x_alpha_ao_new_new(:,:,istate) = potential_x_alpha_ao_new_new(:,:,istate) + tmp_x_a(:,:,istate)
-     potential_c_beta_ao_new(:,:,istate)  =  potential_c_beta_ao_new(:,:,istate) + tmp_c_b(:,:,istate)
-     potential_x_beta_ao_new(:,:,istate)  =  potential_x_beta_ao_new(:,:,istate) + tmp_x_b(:,:,istate)
+       do i = 1, ao_num
+        ao_matrix(i,l) = aos_array(i)
+        do m = 1,3
+         grad_aos_array_transpose(i,m)=grad_aos_array(m,i)
+        enddo
+       enddo
+       do m = 1,3
+        do i = 1,ao_num
+         grad_ao_matrix(i,l,m) = grad_aos_array_transpose(i,m)
+        enddo
+       enddo
+       weight = final_weight_functions_at_grid_points(l,j_rad,i_nucl) 
+       do istate=1,N_states
+
+        energy_x(istate) += weight *  ex(istate)
+        energy_c(istate) += weight *  ec(istate)
+        vx_rho_a(istate) *= weight
+        vc_rho_a(istate) *= weight
+        vx_rho_b(istate) *= weight
+        vc_rho_b(istate) *= weight
+
+        do m= 1,3
+         contrib_grad_xa(m,istate) = weight * (2.d0 * vx_grad_rho_a_2(istate) *  grad_rho_a(m,istate) + vx_grad_rho_a_b(istate)  * grad_rho_b(m,istate)) 
+         contrib_grad_xb(m,istate) = weight * (2.d0 * vx_grad_rho_b_2(istate) *  grad_rho_b(m,istate) + vx_grad_rho_a_b(istate)  * grad_rho_a(m,istate)) 
+         contrib_grad_ca(m,istate) = weight * (2.d0 * vc_grad_rho_a_2(istate) *  grad_rho_a(m,istate) + vc_grad_rho_a_b(istate)  * grad_rho_b(m,istate)) 
+         contrib_grad_cb(m,istate) = weight * (2.d0 * vc_grad_rho_b_2(istate) *  grad_rho_b(m,istate) + vc_grad_rho_a_b(istate)  * grad_rho_a(m,istate)) 
+        enddo 
+
+        do i = 1, ao_num
+         ao_matrix_vc_a(i,l,istate) = vc_rho_a(istate) * aos_array(i)
+         ao_matrix_vc_b(i,l,istate) = vc_rho_b(istate) * aos_array(i)
+         ao_matrix_vx_a(i,l,istate) = vx_rho_a(istate) * aos_array(i)
+         ao_matrix_vx_b(i,l,istate) = vx_rho_b(istate) * aos_array(i)
+         do m = 1,3
+          ao_matrix_dvc_a(i,l,m,istate) = contrib_grad_ca(m) * aos_array(i) 
+          ao_matrix_dvc_b(i,l,m,istate) = contrib_grad_ca(m) * aos_array(i) 
+          ao_matrix_dvx_a(i,l,m,istate) = contrib_grad_xa(m) * aos_array(i) 
+          ao_matrix_dvx_b(i,l,m,istate) = contrib_grad_xb(m) * aos_array(i) 
+          grad_ao_matrix_dvc_a(i,l,m,istate) = contrib_grad_ca(m) * grad_aos_array_transpose(i,m) 
+          grad_ao_matrix_dvc_b(i,l,m,istate) = contrib_grad_ca(m) * grad_aos_array_transpose(i,m) 
+          grad_ao_matrix_dvx_a(i,l,m,istate) = contrib_grad_xa(m) * grad_aos_array_transpose(i,m) 
+          grad_ao_matrix_dvx_b(i,l,m,istate) = contrib_grad_xb(m) * grad_aos_array_transpose(i,m) 
+         enddo
+        enddo
+       enddo
+      enddo
+      do istate = 1, N_states
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_vc_a(1,1,istate),size(ao_matrix_vc_a,1),ao_matrix,size(ao_matrix,1),0.d0,potential_c_alpha_ao_new(1,1,istate),size(potential_c_alpha_ao_new,1))
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_vx_a(1,1,istate),size(ao_matrix_vx_a,1),ao_matrix,size(ao_matrix,1),0.d0,potential_x_alpha_ao_new(1,1,istate),size(potential_x_alpha_ao_new,1))
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_vc_b(1,1,istate),size(ao_matrix_vc_b,1),ao_matrix,size(ao_matrix,1),0.d0,potential_c_beta_ao_new(1,1,istate),size(potential_c_beta_ao_new,1))
+       call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_vx_b(1,1,istate),size(ao_matrix_vx_b,1),ao_matrix,size(ao_matrix,1),0.d0,potential_x_beta_ao_new(1,1,istate),size(potential_x_beta_ao_new,1))
+       do m=1,3
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_dvc_a(1,1,m,istate),size(ao_matrix_dvc_a,1),grad_ao_matrix(1,1,m),size(grad_ao_matrix,1),1.d0,potential_c_alpha_ao_new(1,1,istate),size(potential_c_alpha_ao_new,1))
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_dvx_a(1,1,m,istate),size(ao_matrix_dvx_a,1),grad_ao_matrix(1,1,m),size(grad_ao_matrix,1),1.d0,potential_x_alpha_ao_new(1,1,istate),size(potential_x_alpha_ao_new,1))
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_dvc_b(1,1,m,istate),size(ao_matrix_dvc_b,1),grad_ao_matrix(1,1,m),size(grad_ao_matrix,1),1.d0,potential_c_beta_ao_new(1,1,istate),size(potential_c_beta_ao_new,1))
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,ao_matrix_dvx_b(1,1,m,istate),size(ao_matrix_dvx_b,1),grad_ao_matrix(1,1,m),size(grad_ao_matrix,1),1.d0,potential_x_beta_ao_new(1,1,istate),size(potential_x_beta_ao_new,1))
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,grad_ao_matrix_dvc_a(1,1,m,istate),size(grad_ao_matrix_dvc_a,1),ao_matrix,size(ao_matrix,1),1.d0,potential_c_alpha_ao_new(1,1,istate),size(potential_c_alpha_ao_new,1))
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,grad_ao_matrix_dvx_a(1,1,m,istate),size(grad_ao_matrix_dvx_a,1),ao_matrix,size(ao_matrix,1),1.d0,potential_x_alpha_ao_new(1,1,istate),size(potential_x_alpha_ao_new,1))
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,grad_ao_matrix_dvc_b(1,1,m,istate),size(grad_ao_matrix_dvc_b,1),ao_matrix,size(ao_matrix,1),1.d0,potential_c_beta_ao_new(1,1,istate),size(potential_c_beta_ao_new,1))
+        call dgemm('N','T',ao_num,ao_num,n_points_integration_angular,1.d0,grad_ao_matrix_dvx_b(1,1,m,istate),size(grad_ao_matrix_dvx_b,1),ao_matrix,size(ao_matrix,1),1.d0,potential_x_beta_ao_new(1,1,istate),size(potential_x_beta_ao_new,1))
+       enddo 
+      enddo 
+
+     endif
+     deallocate(tmp_x_a,tmp_x_b,tmp_c_a,tmp_c_b,aos_array,r)
     enddo
-    deallocate(tmp_x_a,tmp_x_b,tmp_c_a,tmp_c_b,aos_array,r)
    enddo
-  enddo
 
 END_PROVIDER 
+
 
