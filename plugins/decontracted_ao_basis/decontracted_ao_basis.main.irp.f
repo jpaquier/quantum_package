5e0af531c3941a6a92e01372f50add58addda00d
Julien Paquier <jpaquier@noumea.lct.jussieu.fr>
2018-04-04 16:16:07 +0200

    Start of the decontracted AO Basis

---
+++ plugins/decontracted_ao_basis/decontracted_ao_basis.main.irp.f
@@ -0,0 +1,18 @@
+program decontracted_ao_basis
+  implicit none
+  BEGIN_DOC
+! TODO
+  END_DOC
+  integer :: i,j,l,l_type
+ !do i = 1, nucl_num
+ ! print*,'nucleus number ',i
+ ! do l_type = 0,7
+ !  print*,'l_type = ', l_type
+ !  print*,'Nucl_num_l_type_Aos    ',Nucl_num_l_type_Aos(l_type,i)
+ !  do j = 1, Nucl_num_l_type_Aos(l_type,i)
+ !   print*,'Nucl_list_l_type_Aos = ',Nucl_list_l_type_Aos(j,l_type,i) 
+ !  enddo
+ ! enddo 
+ !enddo
+ provide number_of_uniq_expo_per_l_shell_per_atom
+end
