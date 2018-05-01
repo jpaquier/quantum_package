4fd924af4bdd61b59164820665f02a259ed2dd11
jpaquier <jpaquier@lct.jussieu.fr>
2018-05-01 18:54:10 +0200

    second phase of checking the coulomb interaction potential for small component basis

--- plugins/Dirac_SCF/Dirac_SCF.main.irp.f
+++ plugins/Dirac_SCF/Dirac_SCF.main.irp.f
@@ -29,24 +29,48 @@ program Dirac_SCF
 ! enddo
 !enddo
 
+!print*,'ao_num =', ao_num
+!do i = 1, ao_num
+!print*,'*********************************************'
+! print*,'nucleus =', ao_nucl(i) 
+!!print*,'nuclear_coordinate =', nucl_coord( ao_nucl(i), 1 ), nucl_coord( ao_nucl(i), 2 ), nucl_coord(ao_nucl(i), 3 )
+! print*,'ao_expo_ordered_transp =', ao_expo_ordered_transp(1,i)
+! print*,'ao_power =', ao_power(i,1), ao_power(i,2), ao_power(i,3) 
+!!print*,'ao_coef_normalized_ordered_transp =', ao_coef_normalized_ordered_transp(1,i)
+! print*,'******************'
+!  do j= 1, ao_num
+!    print*,'nucleus =', ao_nucl(j) 
+!   !print*,'nuclear_coordinate =', nucl_coord( ao_nucl(j), 1 ), nucl_coord( ao_nucl(j), 2 ), nucl_coord(ao_nucl(j), 3 )
+!    print*,'ao_expo_ordered_transp =', ao_expo_ordered_transp(1,j)
+!    print*,'ao_power =', ao_power(j,1), ao_power(j,2), ao_power(j,3)  
+!   !print*,'ao_coef_normalized_ordered_transp =', ao_coef_normalized_ordered_transp(1,j)
+!    print*,'ao_overlap =', ao_overlap(i,j)
+!    print*,'ao_nucl_elec_integral =', ao_nucl_elec_integral(i,j)
+!    print*,'******************'
+! enddo
+!enddo
+
+
  print*,'small_ao_num =', small_ao_num
  do i = 1, small_ao_num 
  print*,'*********************************************'
-  print*,'nucleus =',small_ao_nucl(i), 'nuclear_coordinate =', nucl_coord( small_ao_nucl(i), 1 ), nucl_coord( small_ao_nucl(i), 2 ), nucl_coord(small_ao_nucl(i), 3 )
+  print*,'nucleus =',small_ao_nucl(i) 
+ !print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(i), 1 ), nucl_coord( small_ao_nucl(i), 2 ), nucl_coord(small_ao_nucl(i), 3 )
   print*, 'small_ao_expo =', small_ao_expo(i)
   print*,'small_ao_power =', small_ao_power(i,1), small_ao_power(i,2), small_ao_power(i,3)
-  print*,'small_ao_coef_normalized =', small_ao_coef_normalized(i) 
+ !print*,'small_ao_coef_normalized =', small_ao_coef_normalized(i) 
   print*,'******************'
    do j= 1, small_ao_num
-   print*,'nucleus =',small_ao_nucl(j), 'nuclear_coordinate =', nucl_coord( small_ao_nucl(j), 1 ), nucl_coord( small_ao_nucl(j), 2 ), nucl_coord(small_ao_nucl(j), 3 )
+   print*,'nucleus =',small_ao_nucl(j) 
+  !print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(j), 1 ), nucl_coord( small_ao_nucl(j), 2 ), nucl_coord(small_ao_nucl(j), 3 )
    print*, 'small_ao_expo =', small_ao_expo(j)
    print*,'small_ao_power =', small_ao_power(j,1), small_ao_power(j,2), small_ao_power(j,3)
-   print*,'small_ao_coef_normalized =', small_ao_coef_normalized(j)
-!  print*,'small_ao_overlap_x =', small_ao_overlap_x(i,j)
-!  print*,'small_ao_overlap_y =', small_ao_overlap_y(i,j)
-!  print*,'small_ao_overlap_z =', small_ao_overlap_z(i,j)
-!  print*,'small_ao_overlap =', small_ao_overlap(i,j)
-   print*,'small_ao_mass_energy =', small_ao_mass_energy(i,j)
+  !print*,'small_ao_coef_normalized =', small_ao_coef_normalized(j)
+  !print*,'small_ao_overlap_x =', small_ao_overlap_x(i,j)
+  !print*,'small_ao_overlap_y =', small_ao_overlap_y(i,j)
+  !print*,'small_ao_overlap_z =', small_ao_overlap_z(i,j)
+   print*,'small_ao_overlap =', small_ao_overlap(i,j)
+  !print*,'small_ao_mass_energy =', small_ao_mass_energy(i,j)
    print*,'small_ao_nucl_elec_integral =', small_ao_nucl_elec_integral(i,j) 
    print*,'******************'
   enddo
