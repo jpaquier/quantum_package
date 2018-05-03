9e6f30c9363f08c015afbc37aaede1cc3df97ef1
jpaquier <jpaquier@lct.jussieu.fr>
2018-05-03 11:27:30 +0200

    First steps toward gaussian nucleus integrals

--- plugins/Dirac_SCF/dirac_pot_ao_ints.irp.f
+++ plugins/Dirac_SCF/dirac_pot_ao_ints.irp.f
@@ -99,3 +99,97 @@
 !    print *,  'AO N-e integrals written to disk'
 !  endif
  END_PROVIDER
+
+ BEGIN_PROVIDER[double precision, teeest, (small_ao_num,small_ao_num)]
+  BEGIN_DOC
+ !This is a teeest, because iand, ior, ishift are a mystery in my mind 
+  END_DOC
+   implicit none
+   double precision               :: alpha , beta 
+   integer                        :: i,j,k
+   do i = 1, small_ao_num
+    do j = 1, small_ao_num
+     teeest = iand(i+j,1)
+    enddo
+   enddo 
+ END_PROVIDER
+
+!double precision function G_V_e_n(a_x,a_y,a_z,b_x,b_y,b_z,alpha,beta)
+!implicit none
+!!!! primitive nuclear attraction between the two primitives centered on the same atom ::
+!!!!!         primitive_1 = x**(a_x) y**(a_y) z**(a_z) exp(-alpha * r**2)
+!!!!!         primitive_2 = x**(b_x) y**(b_y) z**(b_z) exp(- beta * r**2)
+!!!! for gaussian charge density of the nucleus
+!integer :: a_x,a_y,a_z,b_x,b_y,b_z
+!double precision :: alpha ,beta
+!double precision :: V_r, V_phi, V_theta
+!if(iand((a_x+b_x),1)==1.or.iand(a_y+b_y,1)==1.or.iand((a_z+b_z),1)==1)then
+! G_V_e_n = 0.d0
+!else
+! G_V_e_n =   G_V_r(a_x+b_x+a_y+b_y+a_z+b_z+1,alpha+beta)    &
+!&          * V_phi(a_x+b_x,a_y+b_y)                         &
+!&          * V_theta(a_z+b_z,a_x+b_x+a_y+b_y+1)
+!endif
+!end
+!
+!double precision function G_V_r(n,alpha)
+!!!!! calculate the radial part of the nuclear attraction integral which is the following integral :
+!!!     integral on "r" with boundaries ( 0 ; + infinity) of [ erf(G*r)  r**n  exp(-alpha * r**2) ]
+!!!! CAUTION  :: this function requires the constant sqpi = dsqrt(pi)
+!implicit none
+!double precision ::  alpha , fact 
+!integer :: n
+!include 'Utils/constants.include.F'
+!if(iand(n,1)==1)then
+! V_r = 0.5d0 * fact(ishft(n,-1)) / (alpha ** (ishft(n,-1) + 1)) 
+!else
+! V_r = sqpi * fact(n) / fact(ishft(n,-1)) * (0.5d0/sqrt(alpha)) ** (n+1)
+!endif
+!end
+
+
+!double precision function V_phi(n,m)
+!implicit none
+!!!!! calculate the angular "phi" part of the nuclear attraction integral wich is the following integral :
+!!!     integral on "phi" with boundaries ( 0 ; 2 pi) of [ cos(phi) **n  sin(phi) **m ]
+!integer :: n,m, i
+!double precision ::  prod, Wallis
+! prod = 1.d0
+! do i = 0,ishft(n,-1)-1
+!  prod = prod/ (1.d0 + dfloat(m+1)/dfloat(n-i-i-1))
+! enddo
+! V_phi = 4.d0 * prod * Wallis(m)
+!end
+
+!double precision function V_theta(n,m)
+!implicit none
+!!!!! calculate the angular "theta" part of the nuclear attraction integral wich is the following integral :
+!!!     integral on "theta" with boundaries ( 0 ; pi) of [ cos(theta) **n  sin(theta) **m ]
+!integer :: n,m,i
+!double precision ::  Wallis, prod
+!include 'Utils/constants.include.F'
+! V_theta = 0.d0
+! prod = 1.d0
+! do i = 0,ishft(n,-1)-1
+!  prod = prod / (1.d0 + dfloat(m+1)/dfloat(n-i-i-1))
+! enddo
+! V_theta = (prod+prod) * Wallis(m)
+!end
+
+!double precision function Wallis(n)
+!!!!! calculate the Wallis integral :
+!!!     integral on "theta" with boundaries ( 0 ; pi/2) of [ cos(theta) **n ]
+!implicit none
+!double precision :: fact
+!integer :: n,p
+!include 'Utils/constants.include.F'
+!if(iand(n,1)==0)then
+! Wallis = fact(ishft(n,-1))
+! Wallis = pi  * fact(n) / (dble(ibset(0_8,n)) * (Wallis+Wallis)*Wallis) 
+!else
+! p = ishft(n,-1)
+! Wallis = fact(p)
+! Wallis = dble(ibset(0_8,p+p)) * Wallis*Wallis / fact(p+p+1)
+!endif
+!end
+
