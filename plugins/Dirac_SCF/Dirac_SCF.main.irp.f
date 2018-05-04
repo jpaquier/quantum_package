555ab5bc929083530b31a520cafb219fbf284ffd
Julien Paquier <jpaquier@noumea.lct.jussieu.fr>
2018-05-04 11:14:51 +0200

    Bounced back on the hypergeometric

--- plugins/Dirac_SCF/Dirac_SCF.main.irp.f
+++ plugins/Dirac_SCF/Dirac_SCF.main.irp.f
@@ -4,7 +4,8 @@ program Dirac_SCF
 ! Print the small component basis, after unrestricted kinetic balance
   END_DOC
   integer :: i,j,k,l_type,n
-
+  double precision :: fact
+  include 'Utils/constants.include.F'
 
 !print*,''
 !do j = 1,small_ao_num 
@@ -52,7 +53,7 @@ program Dirac_SCF
 
 
  print*,'small_ao_num =', small_ao_num
- do i = 1,5 
+ do i = 1,small_ao_num 
  print*,'*********************************************'
   print*,'nucleus =',small_ao_nucl(i) 
  !print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(i), 1 ), nucl_coord( small_ao_nucl(i), 2 ), nucl_coord(small_ao_nucl(i), 3 )
@@ -60,23 +61,27 @@ program Dirac_SCF
   print*,'small_ao_power =', small_ao_power(i,1), small_ao_power(i,2), small_ao_power(i,3)
  !print*,'small_ao_coef_normalized =', small_ao_coef_normalized(i) 
   print*,'******************'
-   do j= 1,small_ao_num
-   print*,'nucleus =',small_ao_nucl(j) 
-  !print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(j), 1 ), nucl_coord( small_ao_nucl(j), 2 ), nucl_coord(small_ao_nucl(j), 3 )
-   print*, 'small_ao_expo =', small_ao_expo(j)
-   print*,'small_ao_power =', small_ao_power(j,1), small_ao_power(j,2), small_ao_power(j,3)
-  !print*,'small_ao_coef_normalized =', small_ao_coef_normalized(j)
-  !print*,'small_ao_overlap_x =', small_ao_overlap_x(i,j)
-  !print*,'small_ao_overlap_y =', small_ao_overlap_y(i,j)
-  !print*,'small_ao_overlap_z =', small_ao_overlap_z(i,j)
-   print*,'small_ao_overlap =', small_ao_overlap(i,j)
-  !print*,'small_ao_mass_energy =', small_ao_mass_energy(i,j)
-   print*,'small_ao_nucl_elec_integral =', small_ao_nucl_elec_integral(i,j)
-  do k = 1, nucl_num 
-    print*,'small_ao_nucl_elec_integral_per_atom =', small_ao_nucl_elec_integral_per_atom(i,j,k) 
-   enddo 
-  print*,'******************'
-  enddo
+  print*, i, ishft(i,-1), fact(ishft(i,-1)) 
+ ! do j= 1,small_ao_num
+ ! print*,'nucleus =',small_ao_nucl(j) 
+ !!print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(j), 1 ), nucl_coord( small_ao_nucl(j), 2 ), nucl_coord(small_ao_nucl(j), 3 )
+ ! print*, 'small_ao_expo =', small_ao_expo(j)
+ ! print*,'small_ao_power =', small_ao_power(j,1), small_ao_power(j,2), small_ao_power(j,3)
+ !!print*,'small_ao_coef_normalized =', small_ao_coef_normalized(j)
+ !!print*,'small_ao_overlap_x =', small_ao_overlap_x(i,j)
+ !!print*,'small_ao_overlap_y =', small_ao_overlap_y(i,j)
+ !!print*,'small_ao_overlap_z =', small_ao_overlap_z(i,j)
+ ! print*,'small_ao_overlap =', small_ao_overlap(i,j)
+ ! print*,'small_ao_mass_energy =', small_ao_mass_energy(i,j)
+ ! print*,'small_ao_nucl_elec_integral =', small_ao_nucl_elec_integral(i,j)
+ ! do k = 1, nucl_num 
+ !  print*,'small_ao_nucl_elec_integral_per_atom =', small_ao_nucl_elec_integral_per_atom(i,j,k) 
+ ! enddo 
+ ! print*, iand((small_ao_power(i,1)+small_ao_power(j,1)),1)
+ ! print*, iand((small_ao_power(i,2)+small_ao_power(j,2)),1)
+ ! print*, iand((small_ao_power(i,3)+small_ao_power(j,3)),1)
+ !print*,'******************'
+ !enddo
  enddo
 
 
