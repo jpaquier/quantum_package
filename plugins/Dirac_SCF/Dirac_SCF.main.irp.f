c74349c66000b53b85398cdea7726376152a5893
Julien Paquier <jpaquier@noumea.lct.jussieu.fr>
2018-05-14 11:28:47 +0200

    First steps onto the realm of the molecular orbitals. What a mess!

--- plugins/Dirac_SCF/Dirac_SCF.main.irp.f
+++ plugins/Dirac_SCF/Dirac_SCF.main.irp.f
@@ -12,7 +12,9 @@ program Dirac_SCF
  do j = 1,2*(ao_num+small_ao_num) 
   print*,'**************************************************'
   do i = 1,2*(ao_num+small_ao_num)
-   print*, i, j, dirac_ao_mono_elec_integral(i,j)
+   print*, 'dirac_mo_mono_elec_integral =',i, j, dirac_mo_mono_elec_integral(i,j)
+  !print*, 'dirac_ao_mono_elec_integral =',i, j, dirac_ao_mono_elec_integral(i,j)
+  !print*,i,j, dirac_mo_coef(j,i)
   enddo
  enddo
 
@@ -45,37 +47,53 @@ program Dirac_SCF
 !print*,'small_ao_num =', small_ao_num
 !do i = 1,small_ao_num 
 !print*,'*********************************************'
-! print*,'nucleus =',small_ao_nucl(i) 
+!!print*,'nucleus =',small_ao_nucl(i) 
 !!print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(i), 1 ), nucl_coord( small_ao_nucl(i), 2 ), nucl_coord(small_ao_nucl(i), 3 )
-! print*, 'small_ao_expo =', small_ao_expo(i)
-! print*,'small_ao_power =', small_ao_power(i,1), small_ao_power(i,2), small_ao_power(i,3)
+!!print*, 'small_ao_expo =', small_ao_expo(i)
+!!print*,'small_ao_power =', small_ao_power(i,1), small_ao_power(i,2), small_ao_power(i,3)
 !!print*,'small_ao_coef_normalized =', small_ao_coef_normalized(i) 
 ! print*,'******************'
-! print*, i, ishft(i,-1), fact(ishft(i,-1)) 
- ! do j= 1,small_ao_num
- ! print*,'nucleus =',small_ao_nucl(j) 
- !!print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(j), 1 ), nucl_coord( small_ao_nucl(j), 2 ), nucl_coord(small_ao_nucl(j), 3 )
- ! print*, 'small_ao_expo =', small_ao_expo(j)
- ! print*,'small_ao_power =', small_ao_power(j,1), small_ao_power(j,2), small_ao_power(j,3)
- !!print*,'small_ao_coef_normalized =', small_ao_coef_normalized(j)
- !!print*,'small_ao_overlap_x =', small_ao_overlap_x(i,j)
- !!print*,'small_ao_overlap_y =', small_ao_overlap_y(i,j)
- !!print*,'small_ao_overlap_z =', small_ao_overlap_z(i,j)
- ! print*,'small_ao_overlap =', small_ao_overlap(i,j)
- ! print*,'small_ao_mass_energy =', small_ao_mass_energy(i,j)
- ! print*,'small_ao_nucl_elec_integral =', small_ao_nucl_elec_integral(i,j)
- ! do k = 1, nucl_num 
- !  print*,'small_ao_nucl_elec_integral_per_atom =', small_ao_nucl_elec_integral_per_atom(i,j,k) 
- ! enddo 
- ! print*, iand((small_ao_power(i,1)+small_ao_power(j,1)),1)
- ! print*, iand((small_ao_power(i,2)+small_ao_power(j,2)),1)
- ! print*, iand((small_ao_power(i,3)+small_ao_power(j,3)),1)
- !print*,'******************'
- !enddo
+!  do j= 1,small_ao_num
+! !print*,'nucleus =',small_ao_nucl(j) 
+! !print*,'nuclear_coordinate =', nucl_coord( small_ao_nucl(j), 1 ), nucl_coord( small_ao_nucl(j), 2 ), nucl_coord(small_ao_nucl(j), 3 )
+! !print*, 'small_ao_expo =', small_ao_expo(j)
+! !print*,'small_ao_power =', small_ao_power(j,1), small_ao_power(j,2), small_ao_power(j,3)
+! !print*,'small_ao_coef_normalized =', small_ao_coef_normalized(j)
+! !print*,'small_ao_overlap_x =', small_ao_overlap_x(i,j)
+! !print*,'small_ao_overlap_y =', small_ao_overlap_y(i,j)
+! !print*,'small_ao_overlap_z =', small_ao_overlap_z(i,j)
+! !print*,'small_ao_overlap =', small_ao_overlap(i,j)
+! !print*,'small_ao_mass_energy =', small_ao_mass_energy(i,j)
+! !print*,'small_ao_nucl_elec_integral =', small_ao_nucl_elec_integral(i,j)
+! !do k = 1, nucl_num 
+! ! print*,'small_ao_nucl_elec_integral_per_atom =', small_ao_nucl_elec_integral_per_atom(i,j,k) 
+! !enddo 
+!  print*,'small_mo_coef =', small_mo_coef(j,i)
+!  print*,'******************'
+! enddo
 !enddo
 
 
-
+!print*,'ao_num =', ao_num
+!do i = 1,ao_num
+!print*,'*********************************************'
+!print*,'nucleus =',ao_nucl(i)
+!print*,'nuclear_coordinate =', nucl_coord(ao_nucl(i), 1 ), nucl_coord( ao_nucl(i), 2 ), nucl_coord(ao_nucl(i), 3 )
+!print*,'ao_expo_ordered_transp =', ao_expo_ordered_transp(1,i)
+!print*,'ao_power =', ao_power(i,1), ao_power(i,2), ao_power(i,3)  
+!print*,'******************'
+! do j= 1,ao_num
+! !print*,'nucleus =',ao_nucl(j)
+! !print*,'nuclear_coordinate =', nucl_coord(ao_nucl(j), 1 ), nucl_coord(ao_nucl(j), 2 ), nucl_coord(ao_nucl(j), 3 )
+! !print*,'ao_expo_ordered_transp =', ao_expo_ordered_transp(1,j)
+! !print*,'ao_power =', ao_power(j,1), ao_power(j,2), ao_power(j,3) 
+! !print*,'ao_ortho_canonical_coef =', ao_ortho_canonical_coef(j,i)
+! !print*,'ao_ortho_canonical_overlap =', ao_ortho_canonical_overlap(j,i)
+!  print*,'mo_mono_elec_integral =', mo_mono_elec_integral(i,j)
+!  print*,'ao_mono_elec_integral =', ao_mono_elec_integral(i,j)
+!  print*,'******************'
+! enddo
+!enddo
 
 !do i = 1, nucl_num
 ! print*,''
---
+++ plugins/Dirac_SCF/dirac_mo_ints.irp.f
@@ -0,0 +1,14 @@
+ BEGIN_PROVIDER [double complex, dirac_mo_mono_elec_integral,(2*(mo_tot_num+small_mo_tot_num),2*(mo_tot_num+small_mo_tot_num))]
+  implicit none
+  BEGIN_DOC
+  ! array of the mono electronic hamiltonian on the MOs basis
+  ! in the 4x4 component formalism with cartesian basis and 
+  ! the unrestricted kinetic-balance scheme  
+  END_DOC
+    call dirac_ao_to_mo(                                                     &
+        dirac_ao_mono_elec_integral,                                         &
+        size(dirac_ao_mono_elec_integral,1),                                 &
+        dirac_mo_mono_elec_integral,                                         &
+        size(dirac_mo_mono_elec_integral,1)                                  &
+        )
+ END_PROVIDER
