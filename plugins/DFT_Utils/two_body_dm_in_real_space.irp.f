51c5471687fa68b6a6dfde4768dc88a00217642e
Anthony Ferté <aferte@master.lct.jussieu.fr>
2018-05-22 17:30:30 +0200

    does not compile

--- plugins/DFT_Utils/two_body_dm_in_real_space.irp.f
+++ plugins/DFT_Utils/two_body_dm_in_real_space.irp.f
@@ -106,6 +106,8 @@ double precision function on_top_two_dm_in_r(r,istate)
 end
 
 
+
+
 double precision function on_top_two_dm_in_r_sym(r,istate)
  implicit none
  integer, intent(in) :: istate
@@ -343,6 +345,39 @@ end
 
 
 
+double precision function on_top_two_dm_in_r_mu_corrected(mu,r,istate)
+ implicit none
+ integer, intent(in) :: istate
+ double precision, intent(in) :: r(3),mu
+ double precision :: mos_array_r(mo_tot_num),pi
+ integer :: i,j,k,l
+ double precision :: accu1,accu2,accu3,accu4,threshold
+ threshold = 1.d-10
+ pi = 4d0 * datan(1d0)
+ call give_all_mos_at_r(r,mos_array_r) 
+
+ on_top_two_dm_in_r_mu_corrected = 0.d0
+ do i = 1, mo_tot_num
+! accu1 = dabs(mos_array_r(i))
+! if(accu1.lt.threshold)cycle
+  do j = 1, mo_tot_num
+! accu2 = accu1 * dabs(mos_array_r(j))
+! if(accu2.lt.threshold)cycle
+   do k = 1, mo_tot_num
+!  accu3 = accu2 *  dabs(mos_array_r(k))
+!  if(accu3.lt.threshold)cycle
+    do l = 1, mo_tot_num
+!    accu4 = accu3 *  dabs(mos_array_r(l))
+!    if(accu4.lt.threshold)cycle
+     on_top_two_dm_in_r_mu_corrected += two_bod_alpha_beta_mo_transposed(l,k,j,i,istate) * mos_array_r(i) * mos_array_r(l) * mos_array_r(k) * mos_array_r(j)
+    enddo
+   enddo
+  enddo
+ enddo
+ on_top_two_dm_in_r_mu_corrected = on_top_two_dm_in_r_mu_corrected / ( 1d0 + 2d0/(dsqrt(pi)*mu) )
+ on_top_two_dm_in_r_mu_corrected = max(on_top_two_dm_in_r_mu_corrected,1.d-15)
+end
+
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -390,7 +425,7 @@ end
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
- subroutine give_epsilon_c_md_on_top_PBE(mu,r,eps_c_md_on_top_PBE)
+subroutine give_epsilon_c_md_on_top_PBE(mu,r,eps_c_md_on_top_PBE)
   implicit none
   double precision, intent(in)  :: mu , r(3)
   double precision, intent(out) :: eps_c_md_on_top_PBE(N_states)
@@ -424,7 +459,7 @@ end
    eps_c_md_on_top_PBE(istate)=e_PBE(istate)/(1d0+beta(istate)*mu**3d0)
   enddo
  end
- 
+
 
  BEGIN_PROVIDER [double precision, Energy_c_md_on_top_PBE, (N_states)]
  BEGIN_DOC
@@ -496,6 +531,76 @@ end
  print*,wall1 - wall0
  
  END_PROVIDER
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+subroutine give_epsilon_c_md_on_top_PBE_mu_corrected(mu,r,eps_c_md_on_top_PBE)
+  implicit none
+  double precision, intent(in)  :: mu , r(3)
+  double precision, intent(out) :: eps_c_md_on_top_PBE(N_states)
+  double precision :: two_dm_in_r, pi, e_pbe(N_states),beta(N_states),on_top_two_dm_in_r_mu_corrected
+  double precision :: aos_array(ao_num), grad_aos_array(3,ao_num)
+  double precision :: rho_a(N_states),rho_b(N_states)
+  double precision :: grad_rho_a(3,N_states),grad_rho_b(3,N_states)
+  double precision :: grad_rho_a_2(N_states),grad_rho_b_2(N_states),grad_rho_a_b(N_states)
+  double precision :: rhoc,rhoo,sigmacc,sigmaco,sigmaoo,vrhoc,vrhoo,vsigmacc,vsigmaco,vsigmaoo
+  integer :: m, istate
+  pi = 4d0 * datan(1d0)
+
+  eps_c_md_on_top_PBE = 0d0
+  call density_and_grad_alpha_beta_and_all_aos_and_grad_aos_at_r(r,rho_a,rho_b, grad_rho_a, grad_rho_b, aos_array, grad_aos_array)
+  grad_rho_a_2 = 0.d0
+  grad_rho_b_2 = 0.d0
+  grad_rho_a_b = 0.d0
+  do istate = 1, N_states
+   do m = 1, 3
+    grad_rho_a_2(istate) += grad_rho_a(m,istate)*grad_rho_a(m,istate)
+    grad_rho_b_2(istate) += grad_rho_b(m,istate)*grad_rho_b(m,istate)
+    grad_rho_a_b(istate) += grad_rho_a(m,istate)*grad_rho_b(m,istate)
+   enddo
+  enddo
+  do istate = 1, N_states
+   ! convertion from (alpha,beta) formalism to (closed, open) formalism
+   call rho_ab_to_rho_oc(rho_a(istate),rho_b(istate),rhoo,rhoc)
+   call grad_rho_ab_to_grad_rho_oc(grad_rho_a_2(istate),grad_rho_b_2(istate),grad_rho_a_b(istate),sigmaoo,sigmacc,sigmaco)
+   call Ec_sr_PBE(0d0,rhoc,rhoo,sigmacc,sigmaco,sigmaoo,e_PBE(istate))
+   beta(istate) = (3d0*e_PBE(istate))/( (-2d0+sqrt(2d0))*sqrt(2d0*pi)*2d0*on_top_two_dm_in_r_mu_corrected(mu,r,istate) )
+   eps_c_md_on_top_PBE(istate)=e_PBE(istate)/(1d0+beta(istate)*mu**3d0)
+  enddo
+ end
+ 
+
+ BEGIN_PROVIDER [double precision, Energy_c_md_on_top_PBE_mu_corrected, (N_states)]
+ BEGIN_DOC
+  ! Give the Ec_md energy with a good large mu behaviour in function of the on top pair density with mu correction coupled to the PBE correlation energy at mu=0
+  ! Ec_md_on_top_PBE = Int epsilon_c_PBE_mu=0 / ( 1 + beta*mu**3 ) = Int eps_c_md_on_top_PBE  with beta chosen to recover the good large mu behaviour of the Energy_c_md_on_top functional
+ END_DOC
+ implicit none
+ double precision :: eps_c_md_on_top_PBE(N_states)
+ double precision :: two_dm_in_r, r(3)
+ double precision :: weight,mu
+ integer :: j,k,l,istate
+ mu = mu_erf
+ Energy_c_md_on_top_PBE_mu_corrected = 0d0
+  
+ do j = 1, nucl_num
+  do k = 1, n_points_radial_grid  -1
+   do l = 1, n_points_integration_angular 
+    r(:) = grid_points_per_atom(:,l,k,j)
+    weight = final_weight_functions_at_grid_points(l,k,j) 
+    call give_epsilon_c_md_on_top_PBE_mu_corrected(mu,r,eps_c_md_on_top_PBE)
+    do istate = 1, N_states
+     Energy_c_md_on_top_PBE_mu_corrected(istate) += eps_c_md_on_top_PBE(istate) * weight
+     print*, Energy_c_md_on_top_PBE_mu_corrected(1)
+    enddo
+   enddo
+  enddo
+ enddo
+ END_PROVIDER
+
+
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
