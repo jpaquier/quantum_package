dde687971eb6d5326fed031d1f017216955d7119
Barthélémy Pradines <pradines@master.lct.jussieu.fr>
2018-06-08 15:22:48 +0200

    added grid_spherical_general.irp.f and print_info_mu_of_r.irp.f

--- plugins/DFT_Utils/grid_spherical_general.irp.f
+++ plugins/DFT_Utils/grid_spherical_general.irp.f
@@ -6,7 +6,7 @@ BEGIN_PROVIDER [integer, n_points_radial_grid_spherical]
 ! number of radial points per atom for 3d numerical integration, needed for DFT
 ! for example
  END_DOC
- n_points_radial_grid_spherical= 100000
+ n_points_radial_grid_spherical= 1000
  n_points_total_shperical = n_points_radial_grid_spherical*n_points_integration_angular
 END_PROVIDER 
 
@@ -14,6 +14,12 @@ END_PROVIDER
  r_max_grid_spherical=8.d0
  implicit none
  END_PROVIDER 
+
+ BEGIN_PROVIDER [double precision, r_core_sphe]
+ r_core_sphe=0.42
+ implicit none
+ END_PROVIDER
+
 
  BEGIN_PROVIDER [double precision, pas_grid_sphe]
  implicit none
@@ -49,55 +55,14 @@ END_PROVIDER
  END_PROVIDER
 
 
- BEGIN_PROVIDER [double precision, Energy_c_md_LDA_mu_of_r_grille_sphe, (N_states)]
-&BEGIN_PROVIDER [double precision, mu_average_grille_sphe, (N_states)]
- implicit none
- BEGIN_DOC
- ! Corelation energy for the multi determinent short range LDA. PRB 73 155111
- ! 2006
- END_DOC
- integer :: j,i,istate
- double precision, allocatable :: aos_array(:), r(:), rho_a(:), rho_b(:), ec(:)
- logical :: dospin
- double precision :: r2(3),dr2(3),local_potential,r12,dx2,mu,mu_coulomb,coulomb,two_body_dm
- double precision :: threshold
- double precision :: cpu0,cpu1
- dospin = .True. ! JT dospin have to be set to true for open shell
- threshold = 1.d-07
- Energy_c_md_LDA_mu_of_r_grille_sphe = 0.d0
- mu_average_grille_sphe = 0.d0
- allocate(aos_array(ao_num),r(3), rho_a(N_states), rho_b(N_states),ec(N_states))
- call cpu_time(cpu0)
- do i = 1, n_points_radial_grid_spherical
-   do j = 1, n_points_integration_angular 
-    r(1) = spherical_grid_of_r(1,j,i)
-    r(2) = spherical_grid_of_r(2,j,i)
-    r(3) = spherical_grid_of_r(3,j,i)
-    call dm_dft_alpha_beta_and_all_aos_at_r(r,rho_a,rho_b,aos_array)
-    if(dabs(weights_rad_of_r(j,i) *pas_grid_sphe * (rho_a(1)+rho_b(1))).lt.threshold)cycle
-    if(basis_set_hf_potential)then
-     call local_r12_operator_on_hf(r,r,local_potential)
-    else
-     call expectation_value_in_real_space(r,r,local_potential,two_body_dm)
-    endif
-    mu = local_potential * dsqrt(dacos(-1.d0)) * 0.5d0
-    do istate = 1, N_states
-!!!!!!!!! CORRELATION PART
-     call ESRC_MD_LDAERF (mu,rho_a(istate),rho_b(istate),dospin,ec(istate))
-     Energy_c_md_LDA_mu_of_r_grille_sphe(istate) += weights_rad_of_r(j,i) * pas_grid_sphe * ec(istate)
-     mu_average_grille_sphe(istate) += weights_rad_of_r(j,i) * pas_grid_sphe * mu
-    enddo
-   enddo
- enddo
- deallocate(aos_array,r,rho_a,rho_b, ec)
- call cpu_time(cpu1)
- print*,'Time for the ec_md integration :',cpu1-cpu0
-END_PROVIDER
-
  BEGIN_PROVIDER [double precision,Energy_c_md_LDA_mu_of_r_grille_sphe_of_r,(N_states,n_points_radial_grid_spherical)]
+&BEGIN_PROVIDER [double precision,Energy_c_md_LDA_mu_of_r_grille_sphe_of_r_integrated,(N_states)]
 &BEGIN_PROVIDER [double precision, rho_r,(N_states,n_points_radial_grid_spherical)]
 &BEGIN_PROVIDER [double precision,mu_r,(N_states,n_points_radial_grid_spherical)]
 &BEGIN_PROVIDER [double precision,rhomu_r,(N_states,n_points_radial_grid_spherical)]
+&BEGIN_PROVIDER [double precision,rhomu_r_integrated, (N_states)]
+&BEGIN_PROVIDER [double precision,Energy_c_md_LDA_mu_of_r_integrated_core,(N_states)]
+&BEGIN_PROVIDER [double precision,Energy_c_md_LDA_mu_of_r_integrated_valence,(N_states)]
 include 'Utils/constants.include.F'
  implicit none
  integer :: i,j,istate
@@ -112,9 +77,15 @@ include 'Utils/constants.include.F'
  allocate(aos_array(ao_num),r(3), rho_a(N_states),rho_b(N_states),ec(N_states))
  call cpu_time(cpu0)
 !!!!!!spherical integrale!!!!!
-
+ rhomu_r_integrated = 0.d0
+ Energy_c_md_LDA_mu_of_r_integrated_core(:) = 0.d0
+ Energy_c_md_LDA_mu_of_r_integrated_valence(:) = 0.d0
+ Energy_c_md_LDA_mu_of_r_grille_sphe_of_r_integrated = 0.d0 
 do i = 1, n_points_radial_grid_spherical
-    
+   Energy_c_md_LDA_mu_of_r_grille_sphe_of_r(:,i) = 0.d0 
+   rho_r(:,i) = 0.d0
+   mu_r(:,i) = 0.d0
+   rhomu_r(:,i) = 0.d0
    do j = 1, n_points_integration_angular
     r(1) = spherical_grid_of_r(1,j,i)
     r(2) = spherical_grid_of_r(2,j,i)
@@ -140,6 +111,14 @@ do i = 1, n_points_radial_grid_spherical
   do istate = 1, N_states
    rhomu_r(istate,i) = rhomu_r(istate,i) / dble(elec_num)
    mu_r(istate,i) = mu_r(istate,i) /  (list_r(i)*list_r(i) * 4.d0 * pi)
+   rhomu_r_integrated(istate) += rhomu_r(istate,i) * pas_grid_sphe
+   Energy_c_md_LDA_mu_of_r_grille_sphe_of_r_integrated(istate) += Energy_c_md_LDA_mu_of_r_grille_sphe_of_r(istate,i) * pas_grid_sphe
+   if(list_r(i) < r_core_sphe) then
+     Energy_c_md_LDA_mu_of_r_integrated_core(istate) += Energy_c_md_LDA_mu_of_r_grille_sphe_of_r(istate,i) * pas_grid_sphe   
+   Else
+     Energy_c_md_LDA_mu_of_r_integrated_valence(istate) += Energy_c_md_LDA_mu_of_r_grille_sphe_of_r(istate,i) * pas_grid_sphe 
+   endif
+      
   enddo
 enddo
  deallocate(aos_array,r,rho_a,rho_b, ec)
--- plugins/DFT_Utils/print_info_mu_of_r.irp.f
+++ plugins/DFT_Utils/print_info_mu_of_r.irp.f
@@ -8,23 +8,49 @@ end
 
 subroutine test_subou
  implicit none
-double precision :: someee,nbE, nbE_a,nbE_b
-integer :: j,i
+ double precision :: someee,nbE,nbE_core,nbE_valence
+ integer :: j,i
+ character*(128) :: output
+ character*(128) :: filename
+ double precision :: Ecmd, mu_moy
+ integer :: i_unit_output,getUnitAndOpen
+ output=trim(ezfio_filename)//'_mu_of_r_info'
+ output=trim(output)
+ print*,'*****************************'
+ print*,'*****************************'
+ print*,'*****************************'
+ print*,'*****************************'
+ print*,'output = ',trim(output)
+ i_unit_output = getUnitAndOpen(output,'w')
+ print*,'i_unit_output',i_unit_output
+ print*,'*****************************'
+ print*,'*****************************'
+ print*,'*****************************'
+
 
  nbE=0.0
-double precision :: Ecmd, mu_moy
+ nbE_core=0.0
+ nbE_valence=0.0
  do i = 1, n_points_radial_grid_spherical
-  write(33,'(100(F10.5,X))')list_r(i),rho_r(1,i),rhomu_r(1,i),Energy_c_md_LDA_mu_of_r_grille_sphe_of_r(1,i)
-  nbE += rho_r(1,i) *pas_grid_sphe  
-  Ecmd += Energy_c_md_LDA_mu_of_r_grille_sphe_of_r(1,i)*pas_grid_sphe
-  mu_moy +=rhomu_r(1,i)*pas_grid_sphe
+  write(i_unit_output,'(100(F10.5,X))')list_r(i),rho_r(1,i),rhomu_r(1,i),Energy_c_md_LDA_mu_of_r_grille_sphe_of_r(1,i)
+  nbE += rho_r(1,i) *pas_grid_sphe 
+  if(list_r(i)< r_core_sphe) then
+    nbE_core += rho_r(1,i) *pas_grid_sphe 
+  else
+    nbE_valence += rho_r(1,i) *pas_grid_sphe
+  endif 
  enddo
 print*,'nbre delectrons =  ',nbE
+print*,'nbre delectrons core=  ',nbE_core
+print*,'nbre delectrons valence=  ',nbE_valence
 
-write(*, '(A28,X,F16.10)') 'DFT mu(r)correlation nouvelle grille=',Ecmd
-write(*, '(A28,X,F16.10)') 'DFT mu(r) correlation ancienne grille=',Energy_c_md_mu_of_r_LDA
+write(*, '(A28,X,F16.10)') 'DFT mu(r) correlation new grid =',Energy_c_md_LDA_mu_of_r_grille_sphe_of_r_integrated
+write(*, '(A28,X,F16.10)') 'DFT mu(r) correlation old grid =',Energy_c_md_mu_of_r_LDA
 
-write(*, '(A28,X,F16.10)') 'Mu moyen nouveau= ',mu_moy 
-write(*, '(A28,X,F16.10)') 'mu_average old=',mu_average
+write(*, '(A28,X,F16.10)') 'DFT mu correlation core =',Energy_c_md_LDA_mu_of_r_integrated_core
+write(*, '(A28,X,F16.10)') 'DFT mu correlation valence=',Energy_c_md_LDA_mu_of_r_integrated_valence
+
+write(*, '(A28,X,F16.10)') 'Mu moyen new    = ',rhomu_r_integrated
+write(*, '(A28,X,F16.10)') 'mu_average old  = ',mu_average
 
 end
