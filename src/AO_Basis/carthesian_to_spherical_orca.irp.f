f6b99a95055b0cc6b25bdb17fb0500338216bde4
Emmanuel Giner <giner.emmanuel@gmail.com>
2018-10-15 11:17:00 +0200

    come back to functional mode

--- src/AO_Basis/carthesian_to_spherical_orca.irp.f
+++ src/AO_Basis/carthesian_to_spherical_orca.irp.f
@@ -230,114 +230,74 @@ END_PROVIDER
    n_shell_passed += 1
   else if (ao_l(i_start)==1)then
    i_start_0 = i_start
-   n_shell_passed_0 = n_shell_passed
-   do k = 1, 3 
+   do i = 1, 3 
     cartesian_to_spherical_matrix(i_start, n_shell_passed) = 1.d0
     i_start += 1
-    do i = 1, 3
-     do l = i,i 
-      cartesian_to_spherical_matrix(i_start_0+i-1,n_shell_passed-1+k) += cart_to_sphe_1(l,k) !* ao_overlap(i+i_start_0-1,l+i_start_0-1)
-     enddo
-    enddo
-    ao_spherical_l(1) += 1
-    index_ao = ao_spherical_l(1) 
-    list_shell_in_basis(index_ao,1) = n_shell_passed_0
-    n_shell_passed_0 += 1
+    n_shell_passed += 1
    enddo
-   n_shell_passed += 3
   else if (ao_l(i_start)==2)then
    i_start_0 = i_start
-   n_shell_passed_0 = n_shell_passed
-   do k = 1, 5
-    do i = 1, 6
+   do i = 1, 6
+    do k = 1, 5
      do l = 1, 6
       cartesian_to_spherical_matrix(i_start_0+i-1,n_shell_passed-1+k) += cart_to_sphe_2(l,k) * ao_overlap(i+i_start_0-1,l+i_start_0-1)
      enddo
     enddo
-    ao_spherical_l(2) += 1
-    index_ao = ao_spherical_l(1) 
-    list_shell_in_basis(index_ao,2) = n_shell_passed_0
-    n_shell_passed_0 += 1
    enddo
    n_shell_passed += 5
    i_start += 6
   else if (ao_l(i_start)==3)then
    i_start_0 = i_start
-   n_shell_passed_0 = n_shell_passed
-   do k = 1, 7
-    do i = 1, 10
+   do i = 1, 10
+    do k = 1, 7
      do l = 1, 10
       cartesian_to_spherical_matrix(i_start_0+i-1,n_shell_passed-1+k) += cart_to_sphe_3(l,k) * ao_overlap(i+i_start_0-1,l+i_start_0-1)
      enddo
     enddo
-    ao_spherical_l(2) += 1
-    index_ao = ao_spherical_l(3) 
-    list_shell_in_basis(index_ao,3) = n_shell_passed_0
-    n_shell_passed_0 += 1
    enddo
    n_shell_passed += 7
    i_start += 10
   else if (ao_l(i_start)==4)then
    i_start_0 = i_start
-   n_shell_passed_0 = n_shell_passed
-   do k = 1, 9
-    do i = 1, 15
+   do i = 1, 15
+    do k = 1, 9
      do l = 1, 15
       cartesian_to_spherical_matrix(i_start_0+i-1,n_shell_passed-1+k) += cart_to_sphe_4(l,k) * ao_overlap(i+i_start_0-1,l+i_start_0-1)
      enddo
     enddo
-    ao_spherical_l(4) += 1
-    index_ao = ao_spherical_l(1) 
-    list_shell_in_basis(index_ao,4) = n_shell_passed_0
-    n_shell_passed_0 += 1
    enddo
    n_shell_passed += 9
    i_start += 15
   else if (ao_l(i_start)==5)then
    i_start_0 = i_start
-   n_shell_passed_0 = n_shell_passed
-   do k = 1, 11
-    do i = 1, 21
+   do i = 1, 21
+    do k = 1, 11
      do l = 1, 21
       cartesian_to_spherical_matrix(i_start_0+i-1,n_shell_passed-1+k) += cart_to_sphe_5(l,k) * ao_overlap(i+i_start_0-1,l+i_start_0-1)
      enddo
     enddo
-    ao_spherical_l(5) += 1
-    index_ao = ao_spherical_l(1) 
-    list_shell_in_basis(index_ao,5) = n_shell_passed_0
-    n_shell_passed_0 += 1
    enddo
    n_shell_passed += 11
    i_start += 21
   else if (ao_l(i_start)==6)then
    i_start_0 = i_start
-   n_shell_passed_0 = n_shell_passed
-   do k = 1, 13
-    do i = 1, 28
+   do i = 1, 28
+    do k = 1, 13
      do l = 1, 28
       cartesian_to_spherical_matrix(i_start_0+i-1,n_shell_passed-1+k) += cart_to_sphe_6(l,k) * ao_overlap(i+i_start_0-1,l+i_start_0-1)
      enddo
     enddo
-    ao_spherical_l(6) += 1
-    index_ao = ao_spherical_l(1) 
-    list_shell_in_basis(index_ao,6) = n_shell_passed
-    n_shell_passed_0 += 1
    enddo
    n_shell_passed += 13
    i_start += 28
   else if (ao_l(i_start)==7)then
    i_start_0 = i_start
-   n_shell_passed_0 = n_shell_passed
-   do k = 1, 15
-    do i = 1, 36
+   do i = 1, 36
+    do k = 1, 15
      do l = 1, 36
       cartesian_to_spherical_matrix(i_start_0+i-1,n_shell_passed-1+k) += cart_to_sphe_7(l,k) * ao_overlap(i+i_start_0-1,l+i_start_0-1)
      enddo
     enddo
-    ao_spherical_l(7) += 1
-    index_ao = ao_spherical_l(1) 
-    list_shell_in_basis(index_ao,7) = n_shell_passed_0
-    n_shell_passed_0 += 1
    enddo
    n_shell_passed += 15
    i_start += 36
