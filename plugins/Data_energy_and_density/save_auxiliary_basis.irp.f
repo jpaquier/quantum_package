e45976d01ee85e219b903f06cea3e55047fa4e25
Emmanuel Giner <giner.emmanuel@gmail.com>
2018-10-17 14:29:17 +0200

    added print_only_mu_of_r_correction.irp.f

--- plugins/Data_energy_and_density/save_auxiliary_basis.irp.f
+++ plugins/Data_energy_and_density/save_auxiliary_basis.irp.f
@@ -3,5 +3,20 @@ program save_auxiliary_mos
   BEGIN_DOC
 ! TODO
   END_DOC
-  
+ integer :: i,j,k
+ double precision, allocatable  :: mo_coef_new(:,:), R(:,:),eigvalues(:), A(:,:), C(:,:)
+ allocate(mo_coef_new(ao_num,mo_tot_num),R(mo_tot_num,mo_tot_num),eigvalues(mo_tot_num,A(mo_tot_num,mo_tot_num),C(mo_tot_num,mo_tot_num))
+ do i =1, mo_tot_num
+  do j = 1, mo_tot_num 
+   A(j,i) = - one_body_dm_mo_alpha_average(j,i) - one_body_dm_mo_beta_average(j,i)
+  enddo
+ enddo
+ call lapack_diag(eigvalues,R,A,mo_tot_num,mo_tot_num)
+ ! R(i,j) = <i_MO|j_natorb>
+ ! |j_natorb> = \sum_{i_MO} R(i,j) |i_MO>
+ call get_pseudo_inverse(R,mo_tot_num,mo_tot_num,mo_tot_num,C,LDC)
+ ! C(i,j) = <i_natorb|j_MO> 
+ ! |j_MO> = \sum_{i_natorb} C(i,j) |i_natorb>
+ 
+ 
 end
