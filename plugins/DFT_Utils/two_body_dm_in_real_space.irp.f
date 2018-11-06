5019e291d161e11724bb3c54d3a5b8adc08e914a
Barthélémy Pradines <pradines@master.lct.jussieu.fr>
2018-11-06 18:40:32 +0100

    fixed some minor stuffs for printing

--- plugins/DFT_Utils/two_body_dm_in_real_space.irp.f
+++ plugins/DFT_Utils/two_body_dm_in_real_space.irp.f
@@ -570,7 +570,7 @@ subroutine give_epsilon_c_md_on_top_PBE_mu_corrected_UEG_from_two_dm(mu,r,two_dm
  END_PROVIDER
 
 
- BEGIN_PROVIDER [double precision, Energy_c_md_on_top_PBE_mu_corrected_UEG_vector, (N_states)]
+ BEGIN_PROVIDER [double precision, Energy_c_md_on_top_PBE_mu_UEG_vector, (N_states)]
  BEGIN_DOC
   ! Give the Ec_md energy with a good large mu behaviour in function of the on top pair density with mu correction based on the on-top of the UEG coupled to the PBE correlation energy at mu=0
   ! Ec_md_on_top_PBE = Int epsilon_c_PBE_mu=0 / ( 1 + beta*mu**3 ) = Int eps_c_md_on_top_PBE  with beta chosen to recover the good large mu behaviour of the Energy_c_md_on_top functional
@@ -582,7 +582,7 @@ subroutine give_epsilon_c_md_on_top_PBE_mu_corrected_UEG_from_two_dm(mu,r,two_dm
  double precision,allocatable  :: eps_c_md_on_top_PBE(:),two_dm(:)
  allocate(eps_c_md_on_top_PBE(N_states),two_dm(N_states))
  mu = mu_erf
- Energy_c_md_on_top_PBE_mu_corrected_UEG_vector = 0.d0
+ Energy_c_md_on_top_PBE_mu_UEG_vector = 0.d0
   
  do i = 1, n_points_final_grid
   r(1) = final_grid_points(1,i)
@@ -592,12 +592,12 @@ subroutine give_epsilon_c_md_on_top_PBE_mu_corrected_UEG_from_two_dm(mu,r,two_dm
   two_dm(:) = on_top_of_r_vector(i,:)
   call give_epsilon_c_md_on_top_PBE_mu_corrected_UEG_from_two_dm(mu,r,two_dm,eps_c_md_on_top_PBE)
   do istate = 1, N_states
-   Energy_c_md_on_top_PBE_mu_corrected_UEG_vector(istate) += eps_c_md_on_top_PBE(istate) * weight
+   Energy_c_md_on_top_PBE_mu_UEG_vector(istate) += eps_c_md_on_top_PBE(istate) * weight
   enddo
  enddo
  END_PROVIDER
 
- BEGIN_PROVIDER [double precision, Energy_c_md_on_top_PBE_mu_corrected_vector, (N_states)]
+ BEGIN_PROVIDER [double precision, Energy_c_md_on_top_PBE_mu_vector, (N_states)]
  BEGIN_DOC
   ! Give the Ec_md energy with a good large mu behaviour in function of the on top pair density with mu correction based on the on-top of the UEG coupled to the PBE correlation energy at mu=0
   ! Ec_md_on_top_PBE = Int epsilon_c_PBE_mu=0 / ( 1 + beta*mu**3 ) = Int eps_c_md_on_top_PBE  with beta chosen to recover the good large mu behaviour of the Energy_c_md_on_top functional
@@ -609,7 +609,7 @@ subroutine give_epsilon_c_md_on_top_PBE_mu_corrected_UEG_from_two_dm(mu,r,two_dm
  double precision,allocatable  :: eps_c_md_on_top_PBE(:),two_dm(:)
  allocate(eps_c_md_on_top_PBE(N_states),two_dm(N_states))
  mu = mu_erf
- Energy_c_md_on_top_PBE_mu_corrected_vector = 0.d0
+ Energy_c_md_on_top_PBE_mu_vector = 0.d0
   
  do i = 1, n_points_final_grid
   r(1) = final_grid_points(1,i)
@@ -619,7 +619,7 @@ subroutine give_epsilon_c_md_on_top_PBE_mu_corrected_UEG_from_two_dm(mu,r,two_dm
   two_dm(:) = on_top_of_r_vector(i,:)
   call give_epsilon_c_md_on_top_PBE_mu_corrected_from_two_dm(mu,r,two_dm,eps_c_md_on_top_PBE)
   do istate = 1, N_states
-   Energy_c_md_on_top_PBE_mu_corrected_vector(istate) += eps_c_md_on_top_PBE(istate) * weight
+   Energy_c_md_on_top_PBE_mu_vector(istate) += eps_c_md_on_top_PBE(istate) * weight
   enddo
  enddo
  END_PROVIDER
