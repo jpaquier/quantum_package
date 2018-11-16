6850bfa242e8e8a624a454da2f20643eb8073ce1
Emmanuel Giner <giner.emmanuel@gmail.com>
2018-11-16 15:53:13 +0100

    RS_KS_SCF works with RSDFT_Utils

--- plugins/becke_numerical_grid/selected_points.irp.f
+++ plugins/becke_numerical_grid/selected_points.irp.f
@@ -4,7 +4,7 @@ BEGIN_PROVIDER [integer, n_points_final_grid]
 ! number of points which are non zero 
  END_DOC
  integer :: i,j,k,l
- double precision :: density, mos_array(mo_tot_num),r(3)
+ double precision :: r(3)
  n_points_final_grid = 0 
   do j = 1, nucl_num  
    do i = 1, n_points_radial_grid -1 
@@ -12,15 +12,10 @@ BEGIN_PROVIDER [integer, n_points_final_grid]
      r(1) = grid_points_per_atom(1,k,i,j)
      r(2) = grid_points_per_atom(2,k,i,j)
      r(3) = grid_points_per_atom(3,k,i,j)
-     call give_all_mos_at_r(r,mos_array)
-     density = 0.d0
-     do l = 1, elec_alpha_num 
-      density += 2.d0 * mos_array(l)**2
-     enddo
-     density = density * 0.5d0 /dble(elec_alpha_num)
-!    if(dabs(final_weight_functions_at_grid_points(k,i,j) * density ).gt.threshold_grid_dft)then
+     ! condition to maybe select points
+     if(.True.)then
       n_points_final_grid += 1
-!    endif
+     endif
     enddo
    enddo
   enddo
@@ -34,7 +29,7 @@ END_PROVIDER
 &BEGIN_PROVIDER [integer, index_final_points_reverse, (n_points_integration_angular,n_points_radial_grid,nucl_num) ]
  implicit none
  integer :: i,j,k,l,i_count
- double precision :: density, mos_array(mo_tot_num),r(3)
+ double precision :: r(3)
  i_count = 0
   do j = 1, nucl_num  
    do i = 1, n_points_radial_grid -1 
@@ -42,13 +37,8 @@ END_PROVIDER
      r(1) = grid_points_per_atom(1,k,i,j)
      r(2) = grid_points_per_atom(2,k,i,j)
      r(3) = grid_points_per_atom(3,k,i,j)
-     call give_all_mos_at_r(r,mos_array)
-     density = 0.d0
-     do l = 1, elec_alpha_num 
-      density += 2.d0 * mos_array(l)**2
-     enddo
-     density = density * 0.5d0 /dble(elec_alpha_num)
-    !if(dabs(final_weight_functions_at_grid_points(k,i,j) * density ).gt.threshold_grid_dft)then
+     ! condition to maybe select points
+     if(.True.)then
       i_count += 1
       final_grid_points(1,i_count) = grid_points_per_atom(1,k,i,j)
       final_grid_points(2,i_count) = grid_points_per_atom(2,k,i,j)
@@ -58,7 +48,7 @@ END_PROVIDER
       index_final_points(2,i_count) = i
       index_final_points(3,i_count) = j
       index_final_points_reverse(k,i,j) = i_count
-    !endif
+     endif
     enddo
    enddo
   enddo
